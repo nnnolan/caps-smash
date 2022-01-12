4b3546db598cbe406e14f4d351f304dcdbf3eb10
Nolan <pestanonolan@gmail.com>
2022-01-12 08:28:54 -0600

    changes

---
+++ Smash-CAPS/Assets/spinny.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinny : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(0, 0, 50*Time.deltaTime);
+
+
+    }
+}
